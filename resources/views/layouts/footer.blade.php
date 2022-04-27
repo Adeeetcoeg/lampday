c88cc2f67d3a607341e272dea8badb9b24ed780c
Adeeetcoeg <coegpengencuan@gmail.com>
2022-04-27 20:23:23 +0700

    KEGABUTAN YANG BER FAEDAH PREN WKWK

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Create by</b>Aditya Nugroho
+    </div>
+    <strong>Copyright &copy; {{ date('Y') }} <a href="/">{{ $setting->nama_perusahaan }}</a>.</strong> All rights
+    reserved.
+</footer>
